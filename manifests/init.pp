107d4ae60c7670835e2320a81f48479060284bf7
Jim Hansson <JIHA@powel.com>
2016-08-08 13:05:14 +0200

    fixed lint issue

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,26 +36,26 @@
 # Copyright 2015 Jim Hansson, unless otherwise noted.
 #
 class emacs_contextmenu($path = 'C:\ProgramData\chocolatey\lib\Emacs\tools') {
-  registry::value { 
+  registry::value {
     # For some reason I need to prefix key with 32: have not got any good explaination for
     # that but it works, I am guessing that if i was to write to a key that is hidden
     # behind WOW64 it would be redirected there.
     'Emacs Context Menu':
-      key => '32:HKEY_CLASSES_ROOT\*\shell\openwemacs',
+      key     => '32:HKEY_CLASSES_ROOT\*\shell\openwemacs',
       # swedish windows hides this behind (Standard), found out how to do this by reading
       # the manifest for registry::value
-      value => '(default)',
+      value   => '(default)',
       require => Package['emacs'],
-      data => "&GNU Emacs";
+      data    => '&GNU Emacs';
     'Emacs Context Menu icon':
-      key => '32:HKEY_CLASSES_ROOT\*\shell\openwemacs',
-      value => 'Icon',
+      key     => '32:HKEY_CLASSES_ROOT\*\shell\openwemacs',
+      value   => 'Icon',
       require => Package['emacs'],
-      data => "$path\\bin\\emacs.exe,0";
+      data    => "${path}\\bin\\emacs.exe,0";
     'Emacs Context Menu command':
-      key => '32:HKEY_CLASSES_ROOT\*\shell\openwemacs\command',
-      value => '(default)',
+      key     => '32:HKEY_CLASSES_ROOT\*\shell\openwemacs\command',
+      value   => '(default)',
       require => Package['emacs'],
-      data => "$path\\bin\\emacsclientw.exe \"%1\"";
+      data    => "${path}\\bin\\emacsclientw.exe \"%1\"";
   }
 }
